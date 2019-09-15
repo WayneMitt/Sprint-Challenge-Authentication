e6dc8950913e49507ea5eb0cd27071fbec993c13
Wayne Mitt <wmitt85@gmail.com>
2019-09-15 18:55:16 -0500

    first pass

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -4,5 +4,18 @@
 */
 
 module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({message: 'not verified'})
+      } else {
+        req.decodedToken = decodedToken
+        next();
+      }
+    })
+  } else {
   res.status(401).json({ you: 'shall not pass!' });
+  }
 };
