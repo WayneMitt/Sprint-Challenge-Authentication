5817ef77bdf445a050c774dda50698b0c2866d31
Wayne Mitt <wmitt85@gmail.com>
2019-09-15 19:46:31 -0500

    implemented tests

--- auth.test.js
+++ auth.test.js
@@ -1,29 +1,30 @@
-const request = require('supertest');
+// const request = require('supertest');
 
-const server = require('./server');
+// const server = require('./server');
+const db = require('./database/dbConfig')
 
 const Users = require('./auth/auth-model');
 
 describe('the server', () => {
 
     beforeEach(async () => {
-        await db('auth').truncate()
+        await db('users').truncate()
     })
     describe('the insert function', () => {
         it('should insert a new user', async () => {
             const userData = {
                 "username": "Wayne",
                 "password": "pass"}
-            const user = await Users.insert(userData)
-            const users = await db('auth')
-            expect(users.length).toBe(0)
-            expect(users[0].model).toBe('Wayne')
+            const user = await Users.add(userData)
+            const users = await db('users')
+            expect(users.length).toBe(1)
+            expect(users[0].username).toBe('Wayne')
         })
         it('should resolve to newly created user', async () => {
             const userData = {
                 "username": "Wayne",
                 "password": "pass"}
-            const user = await Users.insert(userData)
+            const user = await Users.add(userData)
 
             expect(user).toEqual({ id: 1, name: 'Wayne'})
         })
